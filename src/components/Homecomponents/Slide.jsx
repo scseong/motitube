0e095da7c9ec59b4fbf2e130c8c1140518964d74
안준표 <dkswnsvy2010@naver.com>
2023-12-10 00:24:43 +0900

    style home 중간 커밋

--- src/components/Homecomponents/Slide.jsx
+++ src/components/Homecomponents/Slide.jsx
@@ -4,16 +4,41 @@ import styled from 'styled-components';
 export const Slide = ({ imagePath }) => {
   return (
     <SlideContainer>
-      <img src={imagePath} />
+      <div>
+        <img src={imagePath} />
+      </div>
     </SlideContainer>
   );
 };
 
 const SlideContainer = styled.div`
   display: flex;
-  width: 100%;
+  overflow: hidden;
+  border: 5px solid white;
+  padding: 90px;
+  width: 1515px; /* Golden ratio width (approximately 1.618) */
+  height: 700px; /* Golden ratio height (approximately 1.0) */
+  margin: 20px 1px auto 0px;
+
+  & > div {
+    width: 100%;
+    height: 100%;
+    border: 5px solid white;
+    margin: 5px auto auto 10px;
+    overflow: hidden;
+  }
+
   img {
-    width: 100vw;
-    height: 20vw;
+    width: 50%;
+    height: 70%;
+    object-fit: cover;
+    margin: 60px auto auto 500px;
+    border-radius: 15px;
   }
 `;
+
+// const InnerBox = styled.div`
+
+// const Image = styled.img`
+//   /* Rounded corners for a nicer look */
+// `;
